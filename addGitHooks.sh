4799450fb86ad5d67b904b67d616b5213f8c0741
ayush <58028488+desibabua@users.noreply.github.com>
2020-01-03 23:56:12 +0530

    added  hooks to not commit when there are comments in files

--- addGitHooks.sh
+++ addGitHooks.sh
@@ -3,6 +3,13 @@
 cat <<EOF > .git/hooks/pre-commit 
 eslint src test *.js && npm run allTest
 if [ \$? != 0 ]; then 
+    echo "\n \n check tests ...\nThere are some failing tests...\n"
+    exit 1
+fi
+
+commentCount=\$(grep -nrw ./src/* ./test/* -e "//" | wc -l)
+if [ \$commentCount -gt 0 ]; then 
+    echo "\n\n Clear your comments before commiting....\n\n"
     exit 1
 fi
 EOF
